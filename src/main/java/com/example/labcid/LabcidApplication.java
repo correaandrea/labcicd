d81a9c9def36599d98190ce003123e63976b4aeb
Andrea Correa Arango <andrea.correaa1@udea.edu.co>
2024-09-23 12:00:49 -0500

    Primer commit

---
+++ src/main/java/com/example/labcid/LabcidApplication.java
@@ -0,0 +1,13 @@
+package com.example.labcid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabcidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabcidApplication.class, args);
+	}
+
+}
